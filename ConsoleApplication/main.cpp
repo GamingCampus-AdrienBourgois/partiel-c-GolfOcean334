1386287e62593efd014912e30460238e255faddb
unknown <agathelier@gamingcampus.local>
2024-01-19 13:55:43 +0100

    Premier exercice

--- ConsoleApplication/main.cpp
+++ ConsoleApplication/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Exercice.h"
+#include "Solution1.h"
 
 #ifndef AUTOGRADING
 #include "StudentConfiguration.h"
@@ -20,6 +21,22 @@ int main(int argc, char** argv)
 	// You can test multiple exercices by calling multiple times the function AutograderRunTest
 	ExerciceBase::AutograderRunTest(0, 0);
 	ExerciceBase::AutograderRunTest(0, 1);
+
+	//EXO 1 
+	float convertedValue1 = Solution1::ConvertTemperature(71.97f, TemperatureUnits::KELVIN, TemperatureUnits::CELSIUS);
+	std::cout << "Converted value: " << convertedValue1 << std::endl;
+
+	float convertedValue2 = Solution1::ConvertTemperature(71.97f, TemperatureUnits::KELVIN, TemperatureUnits::FAHRENHEIT);
+	std::cout << "Converted value: " << convertedValue2 << std::endl;
+
+	float convertedValue3 = Solution1::ConvertTemperature(71.97f, TemperatureUnits::CELSIUS, TemperatureUnits::KELVIN);
+	std::cout << "Converted value: " << convertedValue3 << std::endl;
+
+	float convertedValue4 = Solution1::ConvertTemperature(21.56f, TemperatureUnits::CELSIUS, TemperatureUnits::CELSIUS);
+	std::cout << "Converted value: " << convertedValue4 << std::endl;
+
+	float convertedValue5 = Solution1::ConvertTemperature(62.02f, TemperatureUnits::FAHRENHEIT, TemperatureUnits::CELSIUS);
+	std::cout << "Converted value: " << convertedValue5 << std::endl;
 
 	return 0;
 
--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -1,4 +1,5 @@
 #include "Solution1.h"
+#include <iostream>
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
@@ -6,7 +7,46 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	return -1.0f;
+    switch (_from)
+    {
+    case TemperatureUnits::CELSIUS:
+        switch (_to)
+        {
+        case TemperatureUnits::CELSIUS:
+            return _value;
+        case TemperatureUnits::FAHRENHEIT:
+            return (_value * 9 / 5) + 32;
+        case TemperatureUnits::KELVIN:
+            return _value + 273;
+        }
+        break;
+    case TemperatureUnits::FAHRENHEIT:
+        switch (_to)
+        {
+        case TemperatureUnits::CELSIUS:
+            return (_value - 32) * 5 / 9;
+        case TemperatureUnits::FAHRENHEIT:
+            return _value;
+        case TemperatureUnits::KELVIN:
+            return (_value - 32) * 5 / 9 + 273;
+        }
+        break;
+    case TemperatureUnits::KELVIN:
+        switch (_to)
+        {
+        case TemperatureUnits::CELSIUS:
+            return _value - 273;
+        case TemperatureUnits::FAHRENHEIT:
+            return (_value - 273) * 9 / 5 + 32;
+        case TemperatureUnits::KELVIN:
+            return _value;
+        }
+        break;
+    }
+
+    return _value;
 }
+
+
 
 #endif
